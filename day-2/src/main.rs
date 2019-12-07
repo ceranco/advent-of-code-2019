0725184a1de0fb349310e5f7dbc739207f1becf0
Eran Cohen <ceranco@gmail.com>
2019-12-07 23:55:10 +0200

    Implemented day 2 p1.

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,6 +1,7 @@
 use clap::{App, Arg};
 use std::{
     ffi::{OsStr, OsString},
+    fs::read_to_string,
     path::Path,
 };
 
@@ -26,7 +27,67 @@ fn app<'a, 'b>() -> App<'a, 'b> {
         )
 }
 
+#[derive(Clone, Copy)]
+enum Opcode {
+    Add = 1,
+    Multiply = 2,
+    Terminate = 99,
+}
+
+impl Opcode {
+    fn from_i32(i: i32) -> Result<Self, ()> {
+        match i {
+            1 => Ok(Opcode::Add),
+            2 => Ok(Opcode::Multiply),
+            99 => Ok(Opcode::Terminate),
+            _ => Err(()),
+        }
+    }
+}
+
 fn main() {
     let matches = app().get_matches();
-    println!("Hello, world!");
+
+    // open the input file, parse it and get the memory
+    let mut memory: Vec<i32> = read_to_string(matches.value_of_os("input").unwrap())
+        .expect("Failed to read the file")
+        .split(',')
+        .map(|s| s.parse::<i32>().expect("Malformed input"))
+        .collect();
+
+    // restore the gravity assist program (your puzzle input) to the "1202 program alarm" state
+    memory[1] = 12;
+    memory[2] = 2;
+
+    // run the program
+    let mut pc= 0usize;
+    loop {
+        match Opcode::from_i32(memory[pc]) {
+            Ok(opcode) => match opcode {
+                Opcode::Add => {
+                    // get the addresses
+                    let idx1 = memory[pc + 1] as usize;
+                    let idx2 = memory[pc + 2] as usize;
+                    let dst = memory[pc + 3] as usize;
+
+                    // perform the operation
+                    memory[dst] = memory[idx1] + memory[idx2];
+                }
+                Opcode::Multiply => {
+                    // get the addresses
+                    let idx1 = memory[pc + 1] as usize;
+                    let idx2 = memory[pc + 2] as usize;
+                    let dst = memory[pc + 3] as usize;
+
+                    // perform the operation
+                    memory[dst] = memory[idx1] * memory[idx2];
+                }
+                Opcode::Terminate => break,
+            },
+            Err(()) => panic!(),
+        };
+        pc += 4;
+    }
+
+    println!("Memory[0]: {}", memory[0]);
 }
