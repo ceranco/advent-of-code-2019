8cc3e6cc2156b561ca1c25055066334bd98a881f
Eran Cohen <ceranco@gmail.com>
2019-12-07 11:49:58 +0200

    Refactored the code to prepare for implementation of the second algorithm.

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -2,7 +2,7 @@ use clap::{self, App, Arg};
 use std::{
     ffi::{OsStr, OsString},
     fs::File,
-    io::{prelude::*, BufReader},
+    io::{BufRead, BufReader},
     path::Path,
 };
 
@@ -12,6 +12,15 @@ fn is_valid_path(path: &OsStr) -> Result<(), OsString> {
     } else {
         Err(OsString::from("The given path does not exist"))
     }
+}
+
+fn calculate_fuel_p1<T: BufRead>(reader: &mut T) -> i32 {
+    let mut fuel_sum = 0;
+    for line in reader.lines() {
+        let mass: i32 = line.unwrap().parse().unwrap();
+        fuel_sum += (mass / 3) - 2;
+    }
+    fuel_sum
 }
 
 fn main() {
@@ -33,14 +42,10 @@ fn main() {
 
     // open the file and prepare it for reading
     let file = File::open(matches.value_of_os("input").unwrap()).unwrap();
-    let reader = BufReader::new(file);
+    let mut reader = BufReader::new(file);
 
-    // read the file line by line
-    let mut fuel_sum = 0;
-    for line in reader.lines() {
-        let mass: i32 = line.unwrap().parse().unwrap();
-        fuel_sum += (mass / 3) - 2;
-    }
+    // calculate the fueld using the asked-for variation
+    let fuel_sum = calculate_fuel_p1(&mut reader);
 
     println!("The calculated fuel sum is: {}", fuel_sum);
 }
