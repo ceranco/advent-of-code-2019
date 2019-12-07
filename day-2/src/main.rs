d9268b158be64b78045fafb16341fca143941b8a
Eran Cohen <ceranco@gmail.com>
2019-12-07 17:35:55 +0200

    Added basic app skeleton. Added the input file.

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,3 +1,32 @@
+use clap::{App, Arg};
+use std::{
+    ffi::{OsStr, OsString},
+    path::Path,
+};
+
+fn is_valid_path(path: &OsStr) -> Result<(), OsString> {
+    if Path::new(path).exists() {
+        Ok(())
+    } else {
+        Err(OsString::from("The given path does not exist"))
+    }
+}
+
+fn app<'a, 'b>() -> App<'a, 'b> {
+    App::new("Advent of Code Day 2")
+        .about("Emulates an Intcode computer")
+        .arg(
+            Arg::with_name("input")
+                .short("i")
+                .long("input")
+                .help("The path to the input file")
+                .takes_value(true)
+                .required(true)
+                .validator_os(is_valid_path),
+        )
+}
+
 fn main() {
+    let matches = app().get_matches();
     println!("Hello, world!");
 }
