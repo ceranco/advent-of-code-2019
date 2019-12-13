55ff34d2d193cab1e7a8dd025216bf79f01b1f38
Eran Cohen <ceranco@gmail.com>
2019-12-13 16:18:15 +0200

    Refactored the code to prepare for the second algorithm.

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,7 +1,7 @@
 mod math;
 
 use crate::math::*;
-use clap::{App, Arg};
+use clap::{App, AppSettings, Arg, SubCommand};
 use std::{
     ffi::{OsStr, OsString},
     fs::File,
@@ -21,24 +21,45 @@ fn app<'a, 'b>() -> App<'a, 'b> {
     App::new("Advent of Code Day 3")
         .version("1.0")
         .author("Eran Cohen")
-        .about("Returns the nearest intersection point of two wires to their source")
-        .arg(
-            Arg::with_name("input")
-                .short("i")
-                .long("input")
-                .help("The path to the input file")
-                .takes_value(true)
-                .required(true)
-                .validator_os(is_valid_path),
+        .about("Returns the nearest intersection point of two wires relative to their source")
+        .subcommand(
+            SubCommand::with_name("manhattan")
+                .about("Calculates distance using the Manhattan distance algorithm (part 1)")
+                .version("1.0")
+                .author("Eran Cohen")
+                .arg(
+                    Arg::with_name("input")
+                        .short("i")
+                        .long("input")
+                        .help("The path to the input file")
+                        .takes_value(true)
+                        .required(true)
+                        .validator_os(is_valid_path),
+                ),
         )
+        .setting(AppSettings::SubcommandRequiredElseHelp)
 }
 
 fn main() {
+    struct Intersection {
+        point: Point,
+        line1_idx: usize,
+        line2_idx: usize,
+    };
+
     // get the path to the input file
     let matches = app().get_matches();
 
     // prepare the file
-    let file = File::open(matches.value_of_os("input").unwrap()).unwrap();
+    let file = File::open(
+        matches
+            .subcommand()
+            .1
+            .unwrap()
+            .value_of_os("input")
+            .unwrap(),
+    )
+    .unwrap();
     let reader = BufReader::new(file);
 
     // parse the file
@@ -67,28 +88,40 @@ fn main() {
         .collect();
 
     // check for intersections
-    let mut intersections: Vec<Point> = vec![];
-    for line1 in &lines[0] {
-        for line2 in &lines[1] {
+    let mut intersections: Vec<Intersection> = vec![];
+    for (idx1, line1) in (&lines[0]).iter().enumerate() {
+        for (idx2, line2) in (&lines[1]).iter().enumerate() {
             if let Some(point) = line1.intersects(line2) {
-                intersections.push(point);
+                intersections.push(Intersection {
+                    point,
+                    line1_idx: idx1,
+                    line2_idx: idx2,
+                });
             }
         }
     }
 
-    let (point, distance) = intersections
-        .iter()
-        .map(|point| (*point, point.x.abs() + point.y.abs()))
-        .fold((Point { x: 0, y: 0 }, std::i32::MAX), |lowest, curr| {
-            if curr.1 != 0 && curr.1 < lowest.1 {
-                curr
-            } else {
-                lowest
-            }
-        });
-
-    println!(
-        "Closest intersection point: {:?}, Manhattan distance: {}",
-        point, distance
-    );
+    // use the manhattan distance algorithm
+    if matches.subcommand_name().unwrap() == "manhattan" {
+        let (point, distance) = intersections
+            .iter()
+            .map(|intersection| {
+                (
+                    intersection.point,
+                    intersection.point.x.abs() + intersection.point.y.abs(),
+                )
+            })
+            .fold((Point { x: 0, y: 0 }, std::i32::MAX), |lowest, curr| {
+                if curr.1 != 0 && curr.1 < lowest.1 {
+                    curr
+                } else {
+                    lowest
+                }
+            });
+    
+        println!(
+            "Closest intersection point: {:?}, Manhattan distance: {}",
+            point, distance
+        );
+    }
 }
