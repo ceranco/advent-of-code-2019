a04341ad52cc4a8398358873faec07304f7834ef
Eran Cohen <ceranco@gmail.com>
2019-12-17 21:17:13 +0200

    Implemented the parsing and traversal code. Finished day-6 p1.

--- day-6/src/main.rs
+++ day-6/src/main.rs
@@ -21,17 +21,29 @@ use std::{
 /// node1.borrow_mut().children.push(node2.clone());
 /// node2.borrow_mut().children.push(node1);
 /// ```
+#[derive(Debug)]
 struct GraphNode {
     children: Vec<Rc<RefCell<GraphNode>>>,
-    identifier: String,
 }
 
 impl GraphNode {
-    fn new(identifier: String) -> Rc<RefCell<GraphNode>> {
+    fn new() -> Rc<RefCell<GraphNode>> {
         Rc::new(RefCell::new(GraphNode {
             children: Vec::new(),
-            identifier,
         }))
+    }
+
+    fn traverse<F: FnMut(i32)>(node: Rc<RefCell<GraphNode>>, proc: F) {
+        fn traverse_impl<F: FnMut(i32)>(node: Rc<RefCell<GraphNode>>, depth: i32, proc: Rc<RefCell<F>>) {
+            // run the proc with the current depth
+            (&mut *proc.borrow_mut())(depth);
+
+            // traverse all the children
+            for child in node.borrow().children.iter() {
+                traverse_impl(child.clone(), depth + 1, proc.clone());
+            }
+        };
+        traverse_impl(node, 0, Rc::new(RefCell::new(proc)));
     }
 }
 
@@ -41,24 +53,34 @@ fn main() {
 
     // open the file and prepare it for parsing
     let file = File::open(opt.path).unwrap();
-    let mut reader = BufReader::new(file);
+    let reader = BufReader::new(file);
 
     // create the graph
     let mut nodes: HashMap<String, Rc<RefCell<GraphNode>>> = HashMap::new();
-    reader.lines().map(|line| line.unwrap()).map(|line| {
-        let stars = line
+    for line in reader.lines() {
+        let mapping = line.unwrap();
+        let mut stars = mapping
             .split(")")
             .map(|identifier| match nodes.get(identifier) {
                 Some(node) => node.clone(),
                 None => {
-                    let node = GraphNode::new(identifier.to_owned());
+                    let node = GraphNode::new();
                     nodes.insert(identifier.to_owned(), node.clone());
                     node
                 }
             });
-        // let center_identifier = tokens.nth(0).unwrap();
-        // let orbiter_identifier = tokens.nth(1).unwrap();
-    });
 
-    println!("Hello, world!");
+        let center = stars.next().unwrap();
+        let orbiter = stars.next().unwrap();
+        center.borrow_mut().children.push(orbiter);
+    }
+
+    // get the center
+    let com = nodes.remove("COM").unwrap();
+
+    // recursive traveral
+    let mut counter: i32 = 0;
+    GraphNode::traverse(com, |depth| counter += depth);
+
+    println!("Checksum: {}", counter);
 }
