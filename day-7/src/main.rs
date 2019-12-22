c00d99c2c8b83be5636d1742edcc49ec4d2e7e1f
Eran Cohen <ceranco@gmail.com>
2019-12-22 16:28:08 +0200

    In middle of work.

--- day-7/src/main.rs
+++ day-7/src/main.rs
@@ -1,7 +1,17 @@
 mod app;
 use app::*;
+use intcode::*;
+use std::fs::read_to_string;
 
 fn main() {
+    // load the program
     let opt: Opt = app().get_matches().into();
+    let memory: Vec<i32> = read_to_string(opt.path)
+        .expect("Failed to read the file")
+        .trim()
+        .split(',')
+        .map(|s| s.parse::<i32>().expect("Malformed input"))
+        .collect();
+
     println!("Opt: {:?}", opt);
 }
